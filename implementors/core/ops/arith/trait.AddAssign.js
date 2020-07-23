86502a51db7e1eed36dfa4947e7ffba886bc53cc
JEnoch <JEnoch@users.noreply.github.com>
2020-07-23 17:37:58 +0000

    deploy: 02cbc595ca823c889325afc4d813158873cec95b

---
+++ implementors/core/ops/arith/trait.AddAssign.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["uhlc"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.AddAssign.html\" title=\"trait core::ops::arith::AddAssign\">AddAssign</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u64.html\">u64</a>&gt; for <a class=\"struct\" href=\"uhlc/struct.NTP64.html\" title=\"struct uhlc::NTP64\">NTP64</a>","synthetic":false,"types":["uhlc::ntp64::NTP64"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["uhlc"] = {"name":"","files":["lib.rs","ntp64.rs","timestamp.rs"]};
+createSourceSidebar();
