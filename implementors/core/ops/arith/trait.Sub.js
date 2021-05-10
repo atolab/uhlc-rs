8e90af418fcd4c77eb332714e387abb3e5f47f69
OlivierHecart <OlivierHecart@users.noreply.github.com>
2021-05-10 13:51:56 +0000

    deploy: 296b063b8717c5b071f19a9b075bd01f732623b1

--- implementors/core/ops/arith/trait.Sub.js
+++ implementors/core/ops/arith/trait.Sub.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["uhlc"] = [{"text":"impl Sub&lt;NTP64&gt; for NTP64","synthetic":false,"types":[]}];
+implementors["uhlc"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a>&lt;<a class=\"struct\" href=\"uhlc/struct.NTP64.html\" title=\"struct uhlc::NTP64\">NTP64</a>&gt; for <a class=\"struct\" href=\"uhlc/struct.NTP64.html\" title=\"struct uhlc::NTP64\">NTP64</a>","synthetic":false,"types":["uhlc::ntp64::NTP64"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
