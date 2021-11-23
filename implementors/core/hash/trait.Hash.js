8655c9f9e20454d29e8a8be03a38aa80dca4ba20
JEnoch <JEnoch@users.noreply.github.com>
2021-11-23 11:36:17 +0000

    deploy: df568ccfbf69806782a3d7314d02613ba088955b

--- implementors/core/hash/trait.Hash.js
+++ implementors/core/hash/trait.Hash.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["uhlc"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.54.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"uhlc/struct.ID.html\" title=\"struct uhlc::ID\">ID</a>","synthetic":false,"types":["uhlc::id::ID"]}];
+implementors["uhlc"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.56.1/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"uhlc/struct.ID.html\" title=\"struct uhlc::ID\">ID</a>","synthetic":false,"types":["uhlc::id::ID"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
