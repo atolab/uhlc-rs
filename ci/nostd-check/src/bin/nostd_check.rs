4400cb141b35332edbf1cf92e7fb8ffa867199a6
Davide Della Giustina <davide@dellagiustina.com>
2023-02-23 09:01:47 +0000

    Added CI check for no_std

---
+++ ci/nostd-check/src/bin/nostd_check.rs
@@ -0,0 +1,34 @@
+//
+// Copyright (c) 2017, 2020 ADLINK Technology Inc.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+//
+
+#![no_std]
+
+use core::panic::PanicInfo;
+use getrandom::{register_custom_getrandom, Error};
+use linked_list_allocator::LockedHeap;
+#[allow(unused_imports)]
+use uhlc;
+
+#[panic_handler]
+fn dummy_panic_handler(_: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[global_allocator]
+static ALLOCATOR: LockedHeap = LockedHeap::empty();
+
+fn dummy_get_rand(_: &mut [u8]) -> Result<(), Error> {
+    Ok(())
+}
+
+fn main() {
+    register_custom_getrandom!(dummy_get_rand);
+}
