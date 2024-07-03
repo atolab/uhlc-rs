69882dd5e692c6f4e94e5e7a101b3a9a59589530
JEnoch <JEnoch@users.noreply.github.com>
2024-07-03 16:15:44 +0000

    deploy: 34aa0ac998ec7d1c5a3555edd44643bf5a9847e6

--- trait.impl/core/ops/arith/trait.SubAssign.js
+++ trait.impl/core/ops/arith/trait.SubAssign.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"uhlc":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.0/core/ops/arith/trait.SubAssign.html\" title=\"trait core::ops::arith::SubAssign\">SubAssign</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.77.0/std/primitive.u64.html\">u64</a>&gt; for <a class=\"struct\" href=\"uhlc/struct.NTP64.html\" title=\"struct uhlc::NTP64\">NTP64</a>"]]
+"uhlc":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.79.0/core/ops/arith/trait.SubAssign.html\" title=\"trait core::ops::arith::SubAssign\">SubAssign</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.79.0/std/primitive.u64.html\">u64</a>&gt; for <a class=\"struct\" href=\"uhlc/struct.NTP64.html\" title=\"struct uhlc::NTP64\">NTP64</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
